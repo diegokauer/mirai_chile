d5b51fc20f36aaaf7a0b258c7141f6cad1223e96
diegokauer <diego.kauer@ug.uchile.cl>
2025-01-19 23:19:58 -0300

    added train and test functions for model training

---
+++ mirai_chile/test.py
@@ -0,0 +1,29 @@
+import torch
+
+
+def test_model(model, dataset, device, dataloader, dry_run):
+    assert dataset in ["logit", "transformer_hidden", "encoder_hidden"]
+
+    test_loss = 0
+    model.eval()
+
+    with torch.no_grad():
+        for batch_idx, data in enumerate(dataloader):
+
+            data[dataset].to(device)
+
+            for key, val in data["batch"].items():
+                data["batch"][key] = val.to(device)
+
+            logit, _, _ = model(data[dataset])
+            pmf, s = model.head.logit_to_cancer_prob(logit)
+            t = data["time_to_event"].to(device)
+            d = data["cancer"].to(device)
+
+            test_loss += model.loss(pmf, s, t, d).item()
+
+            if dry_run:
+                break
+
+    test_loss /= len(dataloader.dataset)
+    print('\nTest set: Average test loss: {:.4f}\n'.format(test_loss))
---
+++ mirai_chile/train.py
@@ -0,0 +1,28 @@
+def train_model(model, dataset, device, dataloader, optimizer, epoch, dry_run):
+    assert dataset in ["logit", "transformer_hidden", "encoder_hidden"]
+
+    model.train()
+
+    for batch_idx, data in enumerate(dataloader):
+
+        data[dataset].to(device)
+
+        for key, val in data["batch"].items():
+            data["batch"][key] = val.to(device)
+
+        optimizer.zero_grad()
+        logit, _, _ = model(data[dataset])
+        pmf, s = model.head.logit_to_cancer_prob(logit)
+        t = data["time_to_event"].to(device)
+        d = data["cancer"].to(device)
+
+        loss = model.loss(pmf, s, t, d)
+        loss.backward()
+        optimizer.stop()
+
+        if batch_idx % 100 == 0:
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                epoch, batch_idx * len(data), len(dataloader.dataset),
+                       100. * batch_idx / len(dataloader), loss.item()))
+            if dry_run:
+                break
